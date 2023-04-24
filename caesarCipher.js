a0c835651cd25f7a144cfef329b4d81341f9e035
katrina keas <katrinakeas@katrinas-MacBook-Pro.local>
2023-04-24 17:19:44 -0500

    variable encodedphrase changes to PhraseOnScreen and encryptedPhrase becomes the id for the html later

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,36 +1,36 @@
-// Caesar Cipher Function
+#!/usr/bin/node
+
+import * as caesarCipher from './caesarCipher.mjs';
+import { encode } from './caesarCipher.mjs';
+import phrases from './phrases.js';
 
 /**
- * Encodes a plaintext string using the Caesar cipher with a given key.
- * 
- * @param {string} plaintext - The plaintext string to be encoded.
- * @param {number} key - The key for the Caesar cipher.
- * @returns {string} The encoded ciphertext string.
+ * Randomly selects a phrase from an array and encodes it using the Caesar cipher with a key of 3.
+ * @param {array} phrases - An array of phrases to choose from.
+ * @returns {string} - The encoded phrase.
  */
-function encode(plaintext, key) {
-    let ciphertext = '';
-    for (let i = 0; i < plaintext.length; i++) {
-      let charCode = plaintext.charCodeAt(i);
-      if (charCode >= 65 && charCode <= 90) {
-        ciphertext += String.fromCharCode(((charCode - 65 + key) % 26) + 65);
-      } else if (charCode >= 97 && charCode <= 122) {
-        ciphertext += String.fromCharCode(((charCode - 97 + key) % 26) + 97);
-      } else {
-        ciphertext += plaintext.charAt(i);
-      }
-    }
-    return ciphertext;
+function selectRandomPhrase(phrases) {
+  // Select a random phrase from the array
+  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  // Encode the phrase using the Caesar cipher with a key of 3
+  let encodedPhrase = caesarCipher.encode(randomPhrase, 3);
+  return encodedPhrase;
 }
-  
-  /**
-   * Decodes a ciphertext string using the Caesar cipher with a given key.
-   * 
-   * @param {string} ciphertext - The ciphertext string to be decoded.
-   * @param {number} key - The key for the Caesar cipher.
-   * @returns {string} The decoded plaintext string.
-   */
-  function decode(ciphertext, key) {
-    return encode(ciphertext, 26 - key);
-  }
-  
-  export { encode, decode };
+
+selectRandomPhrase(phrases) = PhraseOnScreen;
+
+let PhraseOnScreen = document.getElementById("encryptedPhrase").innerHTML;
+
+// Get the phrase-box element from the DOM
+const phraseBox = document.querySelector('#phrase-box');
+
+// Create a new element to hold the phrase
+const phraseElement = document.createElement('div');
+phraseElement.id = 'phrase';
+phraseElement.textContent = caesarCipher.decode(encodedPhrase, 3);
+
+// Add the phrase element to the phrase-box element
+phraseBox.insertBefore(phraseElement, phraseBox.firstChild);
+
+export { PhraseOnScreen };
+export { randomPhrase };
